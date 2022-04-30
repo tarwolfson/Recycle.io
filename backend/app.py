08b065c3f71aa5a42b4ba21ec18b6713b58c3d74
tarwolfson <35375158+tarwolfson@users.noreply.github.com>
2022-04-30 17:42:41 +0300

    cache updater

--- backend/app.py
+++ backend/app.py
@@ -16,4 +16,4 @@ def get_item(name: str):
 
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
---
+++ cache-updater/main.py
@@ -0,0 +1,24 @@
+import sched
+import time
+import redis
+import pymongo
+import logging
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+s = sched.scheduler(time.time, time.sleep)
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["recycleio"]
+mycol = mydb["items"]
+
+def update():
+    mongo_items = [{"name": x["name"], "value": x["value"] } for x in mycol.find()]
+    for item in mongo_items:
+        r.set(item["name"], item["value"])
+    print(mongo_items)
+
+    s.enter(5, 1, update, ())
+
+
+s.enter(5, 1, update, ())
+s.run()
+logging.info("Started cache updater")
