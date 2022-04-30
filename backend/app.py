9d2f4f50c9bc877e56ef84826cf60629bbbc9d54
tarwolfson <35375158+tarwolfson@users.noreply.github.com>
2022-04-30 14:38:23 +0300

    add directory for frontend and backend

---
+++ backend/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+import redis
+
+app = Flask(__name__)
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/item/<name>")
+def get_item(name: str):
+    r.get(name)
+    return jsonify({ "name": name })
+
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=5000)
