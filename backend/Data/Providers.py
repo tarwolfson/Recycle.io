69f73a24086b40f2bdbd98b5b19ffaffc43e8d3f
tarwolfson <35375158+tarwolfson@users.noreply.github.com>
2022-05-06 21:31:32 +0300

    add hit count to cache

--- backend/Data/Providers.py
+++ backend/Data/Providers.py
@@ -1,5 +1,9 @@
 import json
 import redis
+import logging
+
+logging.basicConfig( level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s")
+log = logging.getLogger("providers")
 
 
 class ItemProviderInterface:
@@ -15,7 +19,16 @@ class ItemProviderRedis(ItemProviderInterface):
         self.redis = redis.Redis(host='redis', port=6379, db=0)
 
     def get(self, name: str) -> dict:
-        return json.loads(self.redis.get(name).decode())
+        res = self.redis.get(name)
+        
+        if res:
+            record = json.loads(res.decode())
+            log.info(record)
+            record["hits"] = record["hits"] + 1
+            self.redis.set(name, json.dumps(record))
+            return record
+        
+        return {}
     
     def get_all(self) -> dict:
         return [json.loads(self.redis.get(item).decode()) for item in self.redis.scan_iter()]
--- cache-updater/main.py
+++ cache-updater/main.py
@@ -10,13 +10,16 @@ s = sched.scheduler(time.time, time.sleep)
 myclient = pymongo.MongoClient("mongodb://mongodb:27017/", username="recycleio", password="S3cret")
 mydb = myclient["recycleio"]
 mycol = mydb["items"]
+logging.basicConfig( level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(message)s")
+log = logging.getLogger("cache-updater")
 
 def update():
+    log.info("Updating cache...")
     mongo_items = [x for x in mycol.find()]
     for item in mongo_items:
         del item["_id"]
-        r.set(item["name"], json.dumps(item))
-    print(mongo_items)
+        item["hits"] = 0
+        r.setnx(item["name"], json.dumps(item))
 
     s.enter(5, 1, update, ())
 
